543cdd6c2d5845c2efd54c9f75c4230f581dd69e
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-23 23:50:36 +0300

    Added to project: fontawesome, bootstrap, sass. Configured webpack, edited base view, rebased front resources like: css, js, images, fonts etc.

---
+++ assets/section-front.js
@@ -0,0 +1,6 @@
+import './frontend/css/libs.scss';
+
+import './frontend/css/custom.css';
+import './frontend/css/slick.css';
+import './frontend/css/slick-theme.css';
+import './frontend/css/templatemo.css';
