1f87bee2d838e49c0e70efd52334609a6fcde622
Sergey Kashurin <sergey-kashurin@mail.ru>
2022-02-24 19:34:02 +0300

    Configure js in webpack

---
+++ assets/section-frontend.js
@@ -0,0 +1,13 @@
+import './frontend/css/libs.scss';
+
+import './frontend/css/custom.css';
+import './frontend/css/slick.css';
+import './frontend/css/slick-theme.css';
+import './frontend/css/templatemo.css';
+
+require('./frontend/js/bootstrap.bundle.min');
+require('./frontend/js/custom');
+require('./frontend/js/jquery-1.11.0.min');
+require('./frontend/js/jquery-migrate-1.2.1.min');
+require('./frontend/js/slick.min');
+require('./frontend/js/templatemo');
